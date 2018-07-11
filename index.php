1aa1318b59268c66ffaa57ff94d0216a07de75a5
viveksohal08 <37658624+viveksohal08@users.noreply.github.com>
2018-07-11 15:26:33 +0530

    Header of index.php created.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<title>Student Management System</title>
+		<!-- Bootstrap -->
+		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+		<link href="css/stylesheet.css" rel="stylesheet" />
+	</head>
+	<body>
+		<!-- This is the header section -->
+		<div class="container-fluid" id="header" style="padding-left: 5px;">	 
+		<!-- Add logo and heading in this section -->
+			<a href="index.php"><img id="logo" src="images/logo.jpg" alt="Logo" /></a>
+			<h3>Student Management</h3>		 
+		</div>
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="assets/jquery.min.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+		</script>
+	</body>
+</html>
